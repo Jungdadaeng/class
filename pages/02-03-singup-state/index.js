b855413bc3373be2c04d9b378737588f74ae56ab
jungdadaeng <jungdayoonyi@gmail.com>
2023-03-24 20:48:53 +0900

    signup-alert

--- pages/02-03-singup-state/index.js
+++ pages/02-03-singup-state/index.js
@@ -12,6 +12,13 @@ export default function SignUpStatePage(){
     }
     function onClickSignup(){
         console.log(email, pwd)
+
+        if(email.includes('@') === false){
+            alert('이메일이 올바르지 않습니다. @가 없습니다.')
+        }else{
+            //메시지 알림 이전 , 백엔드 api 함수 호출
+            alert('회원가입을 축하합니다!')
+        }
     }
     return(
         <>
