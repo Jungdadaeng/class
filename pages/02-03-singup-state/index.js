a1af3f2a244ed3a0e70adee9942f2d5e3e5282ed
jungdadaeng <jungdayoonyi@gmail.com>
2023-03-24 20:54:02 +0900

    plus study

--- pages/02-03-singup-state/index.js
+++ pages/02-03-singup-state/index.js
@@ -3,7 +3,8 @@ import { useState } from "react"
 export default function SignUpStatePage(){
     const [email, setEmail] = useState('');
     const [pwd, setPwd] = useState('');
-
+    const [emailError, setEmailError] = useState('');
+   
     function onChangeEmail(event){
         setEmail(event.target.value);
     }
@@ -14,7 +15,9 @@ export default function SignUpStatePage(){
         console.log(email, pwd)
 
         if(email.includes('@') === false){
-            alert('이메일이 올바르지 않습니다. @가 없습니다.')
+            setEmailError("이메일이 올바르지 않습니다. @가 없습니다.")
+            //document.getElementById('error').innerText = "이메일이 올바르지 않습니다. @가 없습니다."
+           // alert('이메일이 올바르지 않습니다. @가 없습니다.')
         }else{
             //메시지 알림 이전 , 백엔드 api 함수 호출
             alert('회원가입을 축하합니다!')
@@ -23,7 +26,8 @@ export default function SignUpStatePage(){
     return(
         <>
             이메일 : <input type="text" onChange={onChangeEmail} />
-            비밀번호 : <input type="password" onChange={onChangePwd}/>
+            <div style={{ color: 'red', fontSize: '5px'}}>{emailError}</div>
+            비밀번호 : <input type="password" onChange={onChangePwd}/><br></br>
             <button onClick={onClickSignup}>회원가입</button>
         </>
     )
