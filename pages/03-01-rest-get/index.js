394356882ed79545fe2aaad676674a3e5bb1ac00
jungdadaeng <jungdayoonyi@gmail.com>
2023-03-25 13:35:50 +0900

    rest-api practice

--- pages/03-01-rest-get/index.js
+++ pages/03-01-rest-get/index.js
@@ -1,6 +1,8 @@
 import axios from "axios"
+import { useState } from "react";
 
 export default function RestGetPage(){
+    const [title, setTitle] = useState('');
     //비동기
     function onClickAsync(){
         const result = axios.get('https://koreanjson.com/posts/1');
@@ -9,7 +11,8 @@ export default function RestGetPage(){
     //동기
     async function onClickSync(){
         const result = await axios.get('https://koreanjson.com/posts/1');
-        console.log('동기', result);
+        console.log('동기', result.data.title);
+        setTitle(result.data.title);
     }
 
 
@@ -17,6 +20,7 @@ export default function RestGetPage(){
         <div>
             <button onClick={onClickAsync}>REST-API 요청하기(비동기)</button>
             <button onClick={onClickSync}>REST-API 요청하기(동기)</button>
+            <div>{title}</div>
         </div>
     )
 }
