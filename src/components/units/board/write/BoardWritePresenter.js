43b1d4460aeb1eb93b16f48950ccd08264838b69
jungdadaeng <jungdayoonyi@gmail.com>
2023-03-29 16:25:48 +0900

    styles seperate

--- src/components/units/board/write/BoardWritePresenter.js
+++ src/components/units/board/write/BoardWritePresenter.js
@@ -1,11 +1,13 @@
+import { RedInput, BlueButton } from "./BoardWrite.styles"
+
 export default function BoardWriteUI(props){
     
     return(
         <>
-            작성자 : <input type="text" onChange={props.onChangeWriter}/><br/>
+            작성자 : <RedInput type="text" onChange={props.onChangeWriter}/><br/>
             제목 : <input type="text" onChange={props.onChangeTitle}/><br/>
             내용 : <input type="text" onChange={props.onChangeContents}/><br/>
-            <button onClick={props.onClickSubmit}>GRAPQL-API 요청하기(동기)</button>
+            <BlueButton onClick={props.onClickSubmit}>GRAPQL-API 요청하기(동기)</BlueButton>
         </>
     )
 }
