3b48bd6115b76613d29d0877fe1629884d9bda8a
jungdadaeng <jungdayoonyi@gmail.com>
2023-04-11 11:05:39 +0900

    tsx file edit , eslint&prettier install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { AppProps} from 'next/app'
 
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
 
   const client = new ApolloClient({
     uri: "http://practice.codebootcamp.co.kr/graphql",
